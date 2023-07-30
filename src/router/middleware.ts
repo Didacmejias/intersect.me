dcb3153039877b47cc6025f980cc1dbf7a21c4c0
Didac <didacmejias@gmail.com>
2023-07-30 12:10:33 +0200

    added types

--- src/router/middleware.ts
+++ src/router/middleware.ts
@@ -1,7 +1,8 @@
 import { useCharacter } from '@/stores/character'
 import { ROUTE_PATH } from './enums'
+import type { NavigationGuardNext, RouteLocation } from 'vue-router'
 
-export const hasCharacter = (to, from, next) => {
+export const hasCharacter = (to: RouteLocation, from: RouteLocation, next: NavigationGuardNext) => {
   if (from.path === ROUTE_PATH.home) return next()
   const {
     character: { nickname }
